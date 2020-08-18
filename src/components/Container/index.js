d721de340b3ceef4804a0dc1b08962a63966b33f
Sandra <sandra.alpha2010@gmail.com>
2020-08-18 11:38:23 -0700

    switched from using local state to hooks

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "../Table";
 import TableHead from "../TableHead"
 import TableBody from "../TableBody"
@@ -8,49 +8,54 @@ import Btn from "../Btn";
 import Search from "../Search";
 import API from "../../utils/API"
 
-class Container extends Component {
-    state = {
-        users: []
-    };
+function Container() {
+    const [users, setUsers] = useState([]);
+    const [button, setButton] = useState("Alphabetize");
 
-    componentDidMount() {
+    useEffect(() => {
         API.getUsers()
             .then(res => {
                 console.log(res);
                 console.log(res.data.results)
-                this.setState({ users: res.data.results });
+                setUsers(res.data.results);
             }).catch(err => console.log(err));
-    };
+    });
+
+    //let's do the search function
+    //put a onChange in Search index, a property that refers to onChange in Btn in render
+    //what i want: as I type a name it renders only those that match the name typed
+    //
+
+    //then the button
 
 
 
-    render() {
-        return (
-            <div className="container">
-                <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, corrupti! Lorem ipsum dolor
+    return (
+        <div className="container">
+            <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, corrupti! Lorem ipsum dolor
             sit amet.</p>
-                <Row>
-                    <Search />
-                    <Btn />
-                </Row>
-                <Table>
-                    <TableHead />
-                    <TableBody>
-                        {this.state.users.map(user => (
-                            <TableBodyItem 
-                                key={user.dob.age}
-                                picture={user.picture.thumbnail}
-                                name={user.name.last}
-                                phone={user.phone}
-                                email={user.email}
-                                dob={user.dob.date}
-                            />
-                        ))}
-                    </TableBody>
-                </Table>
-            </div>
-        );
-    }
+            <Row>
+                <Search />
+                <Btn />
+            </Row>
+            <Table>
+                <TableHead />
+                <TableBody>
+                    {users.map(user => (
+                        <TableBodyItem
+                            key={user.name.last}
+                            picture={user.picture.thumbnail}
+                            name={`${user.name.first} ${user.name.last}`}
+                            phone={user.phone}
+                            email={user.email}
+                            dob={user.dob.date}
+                        />
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+
 };
 
 export default Container;
