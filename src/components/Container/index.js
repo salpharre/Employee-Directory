c4fb84fb61d0e1f1899395ceb02b8fc21aca2d3d
Sandra <sandra.alpha2010@gmail.com>
2020-08-18 15:20:16 -0700

    adding debounce react hook in utils

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -6,25 +6,44 @@ import TableBodyItem from "../TableBodyItem"
 import Row from "../Row";
 import Btn from "../Btn";
 import Search from "../Search";
-import API from "../../utils/API"
+import API from "../../utils/API";
+import useDebounce from "../../utils/DebounceUser"
 
 function Container() {
+    //users holds array of user objects from api and when api is filtered and sorted
+    //searchedUser holds string typed into Search component
+    //buttonText holds default text of button and is changed when button is clicked (changes to "Reset" and back to "Alphabetize" when "Reset" is clicked)
     const [users, setUsers] = useState([]);
-    const [button, setButton] = useState("Alphabetize");
+    const [searchedUser, setSearchedUser] = useState("");
+    const [buttonText, setButtonText] = useState("Alphabetize");
 
     useEffect(() => {
+        loadUsers();
+    });
+
+    function loadUsers() {
         API.getUsers()
             .then(res => {
                 console.log(res);
                 console.log(res.data.results)
                 setUsers(res.data.results);
             }).catch(err => console.log(err));
-    });
-
+    };
     //let's do the search function
     //put a onChange in Search index, a property that refers to onChange in Btn in render
     //what i want: as I type a name it renders only those that match the name typed
-    //
+    const handleInputChange = e => setSearchedUser(e.target.value);
+    
+    const debouncedInput = useDebounce(searchedUser, 400);
+
+    useEffect(() => {
+        if(debouncedInput) {
+            const employee = users.filter(name => {
+                //filter out object that matches the searchedUser
+
+            });
+        } //if need be, add an else here that calls loadUser()
+    }, [debouncedInput]);
 
     //then the button
 
@@ -35,7 +54,10 @@ function Container() {
             <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, corrupti! Lorem ipsum dolor
             sit amet.</p>
             <Row>
-                <Search />
+                <Search 
+                    handleInputChange={handleInputChange}
+                    results={searchedUser}
+                />
                 <Btn />
             </Row>
             <Table>
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Search() {
+function Search(props) {
     return (
         <div className="col-6 d-inline">
-            <input className="form-control mb-3" type="text" placeholder="Default input" />
+            <input
+            value={props.searchedUser} 
+            onChange={props.handleInputChange}
+            className="form-control mb-3" 
+            type="text" 
+            placeholder="Default input" 
+            />
         </div>
     );
 };
 
 export default Search;
+
+/*value={}*/ //in starting input element
